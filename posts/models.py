5bf89eebbdf3a4c006e30ec83598422ad2d2c9c2
kit sitou <ctwohofa@gmail.com>
2025-02-24 06:14:45 -0800

    assignmnet2

--- posts/models.py
+++ posts/models.py
@@ -2,18 +2,30 @@ from django.urls import reverse
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
+class Status(models.Model):
+    name=models.CharField(max_length=128)
+    description=models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=128)
     subtitle = models.CharField(max_length=256)
     body = models.TextField()
     author = models.ForeignKey(
         get_user_model(),
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE #delete key if reocrd is deleted
+    )
+    status=models.ForeignKey(
+        Status,
+        on_delete=models.CASCADE #tell us waht django should when this fkey is deleted
     )
     created_on = models.DateTimeField(auto_now_add=True)
 
-def get_absolute_url(self):
-    return reverse("detail", self=[self.id])
+    def get_absolute_url(self):
+        return reverse("detail", args=[self.id])
 
-def _str_(self):
-    return self.title
+    def __str__(self):
+        return self.title
